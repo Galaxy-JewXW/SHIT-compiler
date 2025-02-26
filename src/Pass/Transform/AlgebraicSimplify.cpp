42de68b75bbcc8730b9f633dd60a332816d6d925
Zhu Xiongwei <Zhu149248@buaa.edu.cn>
2025-02-26 16:26:34 +0800

    :sparkles: feat(CodeOpt): 完成+，-，*，/，%代数化简

--- src/Pass/Transform/AlgebraicSimplify.cpp
+++ src/Pass/Transform/AlgebraicSimplify.cpp
@@ -38,7 +38,7 @@ static bool reduce_add(const std::shared_ptr<Add> &add, std::vector<std::shared_
     if (rhs->is_constant()) {
         // a + 0 = 0
         const auto constant_rhs = std::static_pointer_cast<ConstInt>(rhs);
-        if (std::any_cast<int>(constant_rhs->get_constant_value()) == 0) {
+        if (constant_rhs->is_zero()) {
             add->replace_by_new_value(lhs);
             return true;
         }
@@ -87,7 +87,6 @@ static bool reduce_add(const std::shared_ptr<Add> &add, std::vector<std::shared_
             return true;
         }
     }
-    // 对于a * b + c * d，有以下几种情况
     // b * a + c * a = (b + c) * a
     // a * b + c * a = (b + c) * a
     // a * b + a * c = (b + c) * a
@@ -250,22 +249,241 @@ static bool reduce_sub(const std::shared_ptr<Sub> &sub, std::vector<std::shared_
             }
         }
     }
-    if (rhs->is_constant()) {}
+    if (rhs->is_constant()) {
+        const auto constant_rhs = std::static_pointer_cast<ConstInt>(rhs);
+        // a - 0 = a
+        if (constant_rhs->is_zero()) {
+            sub->replace_by_new_value(lhs);
+            return true;
+        }
+        // (a + c1) - c2 = a + (c1 - c2)
+        if (const auto add_lhs = std::dynamic_pointer_cast<Add>(lhs)) {
+            if (const auto c1 = std::dynamic_pointer_cast<ConstInt>(add_lhs->get_rhs())) {
+                const auto c = std::make_shared<ConstInt>(*c1 - *constant_rhs);
+                const auto new_add = Add::create(Builder::gen_variable_name(), add_lhs->get_lhs(), c, nullptr);
+                replace_instruction(sub, new_add, current_block, instructions, idx);
+                return true;
+            }
+        }
+        if (const auto sub_lhs = std::dynamic_pointer_cast<Sub>(lhs)) {
+            // (a - c1) - c2 = a - (c1 + c2)
+            if (const auto c1 = std::dynamic_pointer_cast<ConstInt>(sub_lhs->get_rhs())) {
+                const auto c = std::make_shared<ConstInt>(*c1 + *constant_rhs);
+                const auto new_sub = Sub::create(Builder::gen_variable_name(), sub_lhs->get_lhs(), c, nullptr);
+                replace_instruction(sub, new_sub, current_block, instructions, idx);
+                return true;
+            }
+            // (c1 - a) - c2 = (c1 - c2) - a
+            if (const auto c1 = std::dynamic_pointer_cast<ConstInt>(sub_lhs->get_lhs())) {
+                const auto c = std::make_shared<ConstInt>(*c1 - *constant_rhs);
+                const auto new_sub = Sub::create(Builder::gen_variable_name(), c, sub_lhs->get_rhs(), nullptr);
+                replace_instruction(sub, new_sub, current_block, instructions, idx);
+                return true;
+            }
+        }
+    }
+    // b * a - c * a = (b - c) * a
+    // a * b - c * a = (b - c) * a
+    // a * b - a * c = (b - c) * a
+    // b * a - a * c = (b - c) * a
+    if (const auto mul_lhs = std::dynamic_pointer_cast<Mul>(lhs), mul_rhs = std::dynamic_pointer_cast<Mul>(rhs);
+        mul_lhs != nullptr && mul_rhs != nullptr) {
+        const auto x = mul_lhs->get_lhs(), y = mul_lhs->get_rhs(),
+                   z = mul_rhs->get_lhs(), w = mul_rhs->get_rhs();
+        if (y == w) {
+            const auto new_sub = Sub::create(Builder::gen_variable_name(), x, z, nullptr);
+            insert_instruction(new_sub, current_block, instructions, idx);
+            const auto new_mul = Mul::create(Builder::gen_variable_name(), new_sub, y, nullptr);
+            replace_instruction(sub, new_mul, current_block, instructions, idx);
+            return true;
+        }
+        if (x == w) {
+            const auto new_sub = Sub::create(Builder::gen_variable_name(), y, z, nullptr);
+            insert_instruction(new_sub, current_block, instructions, idx);
+            const auto new_mul = Mul::create(Builder::gen_variable_name(), new_sub, x, nullptr);
+            replace_instruction(sub, new_mul, current_block, instructions, idx);
+            return true;
+        }
+        if (x == z) {
+            const auto new_sub = Sub::create(Builder::gen_variable_name(), y, w, nullptr);
+            insert_instruction(new_sub, current_block, instructions, idx);
+            const auto new_mul = Mul::create(Builder::gen_variable_name(), new_sub, x, nullptr);
+            replace_instruction(sub, new_mul, current_block, instructions, idx);
+            return true;
+        }
+        if (y == z) {
+            const auto new_add = Sub::create(Builder::gen_variable_name(), x, w, nullptr);
+            insert_instruction(new_add, current_block, instructions, idx);
+            const auto new_mul = Mul::create(Builder::gen_variable_name(), new_add, y, nullptr);
+            replace_instruction(sub, new_mul, current_block, instructions, idx);
+            return true;
+        }
+    }
     return false;
 }
 
 static bool reduce_mul(const std::shared_ptr<Mul> &mul, std::vector<std::shared_ptr<Instruction>> &instructions,
-                       size_t &idx) {
+                       const size_t &idx) {
+    const auto current_block = mul->get_block();
+    const auto lhs = mul->get_lhs(), rhs = mul->get_rhs();
+    if (rhs->is_constant()) [[unlikely]] {
+        const auto constant_rhs = std::static_pointer_cast<ConstInt>(rhs);
+        const auto zero = std::make_shared<ConstInt>(0);
+        // a * 0 = 0
+        if (constant_rhs->is_zero()) {
+            mul->replace_by_new_value(zero);
+            return true;
+        }
+        const int constant_rhs_v = std::any_cast<int>(constant_rhs->get_constant_value());
+        // a * 1 = a
+        if (constant_rhs_v == 1) {
+            mul->replace_by_new_value(lhs);
+            return true;
+        }
+        // a * (-1) = 0 - a
+        if (constant_rhs_v == -1) {
+            const auto new_sub = Sub::create(Builder::gen_variable_name(), zero, lhs, nullptr);
+            replace_instruction(mul, new_sub, current_block, instructions, idx);
+            return true;
+        }
+        // (-a) * c = a * (-c)
+        if (const auto sub_lhs = std::dynamic_pointer_cast<Sub>(lhs)) {
+            if (const auto c1 = std::dynamic_pointer_cast<ConstInt>(sub_lhs->get_lhs());
+                c1 != nullptr && c1->is_zero()) {
+                const auto c = std::make_shared<ConstInt>(-constant_rhs_v);
+                const auto new_mul = Mul::create(Builder::gen_variable_name(), sub_lhs->get_rhs(), c, nullptr);
+                replace_instruction(mul, new_mul, current_block, instructions, idx);
+                return true;
+            }
+        }
+    }
     return false;
 }
 
 static bool reduce_div(const std::shared_ptr<Div> &div, std::vector<std::shared_ptr<Instruction>> &instructions,
-                       size_t &idx) {
+                       const size_t &idx) {
+    const auto current_block = div->get_block();
+    const auto lhs = div->get_lhs(), rhs = div->get_rhs();
+    // a / a = 1
+    if (lhs == rhs) {
+        div->replace_by_new_value(std::make_shared<ConstInt>(1));
+        return true;
+    }
+    // a / (-a) = -1
+    if (const auto sub_rhs = std::dynamic_pointer_cast<Sub>(rhs)) {
+        if (const auto c1 = std::dynamic_pointer_cast<ConstInt>(sub_rhs->get_lhs());
+            c1 != nullptr && c1->is_zero() && sub_rhs->get_rhs() == lhs) {
+            div->replace_by_new_value(std::make_shared<ConstInt>(-1));
+            return true;
+        }
+    }
+    if (lhs->is_constant()) {
+        // 0 / a = 0
+        if (const auto constant_lhs = std::static_pointer_cast<ConstInt>(lhs); constant_lhs->is_zero()) {
+            div->replace_by_new_value(std::make_shared<ConstInt>(0));
+            return true;
+        }
+    }
+    if (rhs->is_constant()) {
+        const auto constant_rhs = std::static_pointer_cast<ConstInt>(rhs);
+        const int constant_rhs_v = std::any_cast<int>(constant_rhs->get_constant_value());
+        // a / 1 = a
+        if (constant_rhs_v == 1) {
+            div->replace_by_new_value(lhs);
+            return true;
+        }
+        // a / (-1) = 0 - a
+        if (constant_rhs_v == -1) {
+            const auto new_sub = Sub::create(Builder::gen_variable_name(), std::make_shared<ConstInt>(0), lhs, nullptr);
+            replace_instruction(div, new_sub, current_block, instructions, idx);
+            return true;
+        }
+        if (const auto mul_lhs = std::dynamic_pointer_cast<Mul>(lhs)) {
+            // (a * c2) / c1 = x * (c2 / c1), when c2 % c1 == 0
+            if (const auto c2 = std::dynamic_pointer_cast<ConstInt>(mul_lhs->get_rhs())) {
+                if (const int c2_v = std::any_cast<int>(c2->get_constant_value()); c2_v % constant_rhs_v == 0) {
+                    const auto c = std::make_shared<ConstInt>(c2_v / constant_rhs_v);
+                    const auto new_mul = Mul::create(Builder::gen_variable_name(), mul_lhs->get_lhs(), c, nullptr);
+                    replace_instruction(div, new_mul, current_block, instructions, idx);
+                    return true;
+                }
+            }
+        }
+        if (const auto sub_lhs = std::dynamic_pointer_cast<Sub>(lhs)) {
+            // (-a) / c = a / (-c)
+            if (const auto c1 = std::dynamic_pointer_cast<ConstInt>(sub_lhs->get_lhs());
+                c1 != nullptr && c1->is_zero()) {
+                const auto c = std::make_shared<ConstInt>(-constant_rhs_v);
+                const auto new_div = Div::create(Builder::gen_variable_name(), sub_lhs->get_rhs(), c, nullptr);
+                replace_instruction(div, new_div, current_block, instructions, idx);
+                return true;
+            }
+        }
+    }
+    // (-a) / a = 1
+    if (const auto sub_lhs = std::dynamic_pointer_cast<Sub>(lhs)) {
+        if (const auto c1 = std::dynamic_pointer_cast<ConstInt>(sub_lhs->get_lhs());
+            c1 != nullptr && c1->is_zero()) {
+            if (sub_lhs->get_rhs() == rhs) {
+                div->replace_by_new_value(std::make_shared<ConstInt>(1));
+                return true;
+            }
+        }
+    }
+    if (const auto mul_rhs = std::dynamic_pointer_cast<Mul>(rhs)) {
+        const auto x = mul_rhs->get_lhs(), y = mul_rhs->get_rhs();
+        // a / (a * b) = 1 / b
+        if (lhs == x) {
+            const auto new_div = Div::create(Builder::gen_variable_name(), std::make_shared<ConstInt>(1), y, nullptr);
+            replace_instruction(div, new_div, current_block, instructions, idx);
+            return true;
+        }
+        // a / (b * a) = 1 / b
+        if (lhs == y) {
+            const auto new_div = Div::create(Builder::gen_variable_name(), std::make_shared<ConstInt>(1), x, nullptr);
+            replace_instruction(div, new_div, current_block, instructions, idx);
+            return true;
+        }
+    }
     return false;
 }
 
 static bool reduce_mod(const std::shared_ptr<Mod> &mod, std::vector<std::shared_ptr<Instruction>> &instructions,
-                       size_t &idx) {
+                       const size_t &idx) {
+    const auto current_block = mod->get_block();
+    const auto lhs = mod->get_lhs(), rhs = mod->get_rhs();
+    // a % a = 0
+    if (lhs == rhs) {
+        mod->replace_by_new_value(std::make_shared<ConstInt>(1));
+        return true;
+    }
+    if (lhs->is_constant()) {
+        // 0 % a = 0
+        if (const auto constant_lhs = std::static_pointer_cast<ConstInt>(lhs); constant_lhs->is_zero()) {
+            mod->replace_by_new_value(std::make_shared<ConstInt>(0));
+            return true;
+        }
+    }
+    if (rhs->is_constant()) {
+        const auto constant_rhs = std::static_pointer_cast<ConstInt>(rhs);
+        const int constant_rhs_v = std::any_cast<int>(constant_rhs->get_constant_value());
+        // a % 1 = 0
+        if (constant_rhs_v == 1 || constant_rhs_v == -1) {
+            mod->replace_by_new_value(std::make_shared<ConstInt>(0));
+            return true;
+        }
+        if (const auto mul_lhs = std::dynamic_pointer_cast<Mul>(lhs)) {
+            // (a * c2) % c1 = x * (c2 % c1), when c2 % c1 == 0
+            if (const auto c2 = std::dynamic_pointer_cast<ConstInt>(mul_lhs->get_rhs())) {
+                if (const int c2_v = std::any_cast<int>(c2->get_constant_value()); c2_v % constant_rhs_v == 0) {
+                    const auto c = std::make_shared<ConstInt>(c2_v % constant_rhs_v);
+                    const auto new_mul = Mul::create(Builder::gen_variable_name(), mul_lhs->get_lhs(), c, nullptr);
+                    replace_instruction(mod, new_mul, current_block, instructions, idx);
+                    return true;
+                }
+            }
+        }
+    }
     return false;
 }
 
@@ -315,4 +533,8 @@ void Pass::AlgebraicSimplify::transform(const std::shared_ptr<Module> module) {
         const auto dead_inst = create<DeadInstEliminate>();
         dead_inst->run_on(module);
     } while (changed);
+    const auto fold = create<ConstantFolding>();
+    fold->run_on(module);
+    const auto dead_inst = create<DeadInstEliminate>();
+    dead_inst->run_on(module);
 }
