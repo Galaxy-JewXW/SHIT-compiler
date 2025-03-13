2cd1b105ec7fce1208d0983225ac55a043ef3fb3
jude <Zhu139248@outlook.com>
2025-03-13 15:33:51 +0800

    fix interpreter

--- include/Mir/Interpreter.h
+++ include/Mir/Interpreter.h
@@ -4,6 +4,7 @@
 #include <unordered_map>
 #include <variant>
 
+#include "Instruction.h"
 #include "Structure.h"
 
 namespace Mir {
@@ -22,18 +23,30 @@ public:
         explicit eval_t(const size_t t) : variant(static_cast<int>(t)) {}
     };
 
+    // args是传入的实参
+    eval_t interpret_function(const std::shared_ptr<Function> &func,
+                              const std::vector<eval_t> &real_args);
+
 private:
     eval_t func_return_value = 0;
     std::shared_ptr<Block> current_block = nullptr;
     std::shared_ptr<Block> prev_block = nullptr;
     std::vector<eval_t> eval_stack{};
     std::unordered_map<std::shared_ptr<Value>, eval_t> value_map{};
-    std::unordered_map<std::shared_ptr<Value>, eval_t> phi_map{};
 
-public:
-    // args是传入的实参
-    eval_t interpret_function(const std::shared_ptr<Function> &func,
-                              const std::vector<eval_t> &real_args);
+    eval_t get_value(const std::shared_ptr<Value> &value);
+
+    void interpret_instruction(const std::shared_ptr<Instruction> &instruction);
+
+    void interpret_br(const std::shared_ptr<Branch> &branch);
+
+    void interpret_jump(const std::shared_ptr<Jump> &jump);
+
+    void interpret_ret(const std::shared_ptr<Ret> &ret);
+
+    void interpret_intbinary(const std::shared_ptr<IntBinary> &binary);
+
+    void interpret_floatbinary(const std::shared_ptr<FloatBinary> &binary);
 };
 }
 
--- src/Mir/Interpreter.cpp
+++ src/Mir/Interpreter.cpp
@@ -1,7 +1,85 @@
 #include "Mir/Interpreter.h"
+
+#include <Mir/Instruction.h>
+
 #include "Utils/Log.h"
 
 namespace Mir {
+Interpreter::eval_t Interpreter::get_value(const std::shared_ptr<Value> &value) {
+    if (value->is_constant()) {
+        if (const auto constant_float = std::dynamic_pointer_cast<ConstFloat>(value)) {
+            return std::any_cast<double>(constant_float->get_constant_value());
+        }
+        if (const auto constant_int = std::dynamic_pointer_cast<ConstInt>(value)) {
+            return std::any_cast<int>(constant_int->get_constant_value());
+        }
+        if (const auto constant_bool = std::dynamic_pointer_cast<ConstBool>(value)) {
+            return std::any_cast<int>(constant_bool->get_constant_value());
+        }
+    }
+    if (value_map.find(value) != value_map.end()) {
+        return value_map[value];
+    }
+    log_fatal("Unknown value %s", value->to_string().c_str());
+}
+
+void Interpreter::interpret_br(const std::shared_ptr<Branch> &branch) {
+    prev_block = current_block;
+    if (get_value(branch->get_cond())) {
+        current_block = branch->get_true_block();
+    } else {
+        current_block = branch->get_false_block();
+    }
+}
+
+void Interpreter::interpret_jump(const std::shared_ptr<Jump> &jump) {
+    prev_block = current_block;
+    current_block = jump->get_target_block();
+}
+
+void Interpreter::interpret_ret(const std::shared_ptr<Ret> &ret) {
+    prev_block = current_block;
+    current_block = nullptr;
+    if (ret->get_type()->is_void()) {
+        func_return_value = 0;
+    } else {
+        func_return_value = get_value(ret->get_value());
+    }
+}
+
+void Interpreter::interpret_intbinary(const std::shared_ptr<IntBinary> &binary) {
+    eval_t res;
+    eval_t left = get_value(binary->get_lhs()), right = get_value(binary->get_rhs());
+    // switch (binary->op) {
+    //     case IntBinary::Op::ADD:
+    //         res = left + right;
+    // }
+}
+
+
+void Interpreter::interpret_instruction(const std::shared_ptr<Instruction> &instruction) {
+    switch (instruction->get_op()) {
+        case Operator::BRANCH:
+            interpret_br(std::static_pointer_cast<Branch>(instruction));
+            break;
+        case Operator::JUMP:
+            interpret_jump(std::static_pointer_cast<Jump>(instruction));
+            break;
+        case Operator::RET:
+            interpret_ret(std::static_pointer_cast<Ret>(instruction));
+            break;
+        case Operator::INTBINARY:
+            interpret_intbinary(std::static_pointer_cast<IntBinary>(instruction));
+            break;
+        case Operator::FLOATBINARY:
+            interpret_floatbinary(std::static_pointer_cast<FloatBinary>(instruction));
+            break;
+        default:
+            log_error("Unhandled instruction type");
+    }
+}
+
+
 Interpreter::eval_t Interpreter::interpret_function(const std::shared_ptr<Function> &func,
                                                     const std::vector<eval_t> &real_args) {
     const auto &arguments = func->get_arguments();
@@ -11,11 +89,12 @@ Interpreter::eval_t Interpreter::interpret_function(const std::shared_ptr<Functi
     for (size_t i = 0; i < arguments.size(); i++) {
         value_map[arguments[i]] = real_args[i];
     }
+    prev_block = nullptr;
     current_block = func->get_blocks().front();
     while (current_block != nullptr) {
-        // for (const auto &instruction : current_block->get_instructions()) {
-        //
-        // }
+        for (const auto &instruction: current_block->get_instructions()) {
+            interpret_instruction(instruction);
+        }
     }
     return func_return_value;
 }
