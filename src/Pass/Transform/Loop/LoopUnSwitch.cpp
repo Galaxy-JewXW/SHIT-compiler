3304b703859ba629f198e91ba70319db25658894
wxm <22373300@buaa.edu.cn>
2025-07-19 05:14:53 +0800

    完善loop_unswitch架构，并补充指令删除的实现办法

--- src/Pass/Transform/Loop/LoopUnSwitch.cpp
+++ src/Pass/Transform/Loop/LoopUnSwitch.cpp
@@ -3,6 +3,7 @@
 #include "Pass/Transforms/Loop.h"
 #include "Pass/Transforms/ControlFlow.h"
 #include "Pass/Transforms/DataFlow.h"
+#include "Pass/Util.h"
 
 namespace Pass {
 
@@ -77,12 +78,52 @@ namespace Pass {
             for (int j = 0;j < branch_vector.size(); j++) {
                 auto cond_instr = new_info->get_value_reflect(branch_vector[j])->as<Mir::Instruction>();
                 auto cond_block = cond_instr->get_block();
-                // fixme : cond_instr->delete_self()
+                cond_block->get_instructions().erase(Utils::inst_as_iter(cond_instr).value());
                 if((i & (1 << (branch_vector.size() - 1 - j))) == 0) {
                     Mir::Jump::create(new_info->get_value_reflect(true_blocks[j])->as<Mir::Block>(), cond_block);
                 }
                 else {
                     Mir::Jump::create(new_info->get_value_reflect(false_blocks[j])->as<Mir::Block>(), cond_block);
+                }
+            }
+
+            auto new_loop_header = new_node->get_loop()->get_header();
+            for (const auto& instr : *new_loop_header->get_phis()) {
+                auto phi = instr->is<Mir::Phi>();
+                phi->modify_operand(last_preheader, cond_blocks.at((i >> 1) - 1));
+            }
+            cond_blocks.push_back(new_loop_header);
+        }
+
+        for (int i = 1; i < (1 << branch_vector.size()); i++) {
+            auto l_block = cond_blocks.at((i << 1) - 1);
+            auto r_block = cond_blocks.at(i << 1);
+            int k = static_cast<int>(std::log2(i)) + 1;
+            Mir::Branch::create(branch_vector.at(k - 1), l_block, r_block, cond_blocks.at(i - 1));
+        }
+        auto instr = last_preheader->get_instructions().back()->as<Mir::Terminator>();
+        instr->modify_operand(node->get_loop()->get_header(), cond_blocks[0]);
+
+        for (const auto& exit_block : node->get_loop()->get_exits()) {
+            for (const auto& phi_instr : *exit_block->get_phis()) {
+                auto phi = phi_instr->is<Mir::Phi>();
+                for (const auto& entry : phi->get_optional_values()) {
+                    if (clone_infos[0]->contain_value(entry.first)) {
+                        phi->remove_optional_value(entry.first);
+                        if (clone_infos[0]->contain_value(entry.second)) {
+                            for (const auto& clone_info : clone_infos) {
+                                auto block = clone_info->get_value_reflect(entry.first)->as<Mir::Block>();
+                                auto value = clone_info->get_value_reflect(entry.second);
+                                phi->set_optional_value(block, value);
+                            }
+                        }
+                        else {
+                            for (const auto& clone_info : clone_infos) {
+                                auto block = clone_info->get_value_reflect(entry.first)->as<Mir::Block>();
+                                phi->set_optional_value(block, entry.second);
+                            }
+                        }
+                    }
                 }
             }
         }
