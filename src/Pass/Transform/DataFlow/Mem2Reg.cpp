01bb9e63ae3d6d7c2906a3a82c457067cb7e5cc1
Zhu Xiongwei <zhu149248@gmail.com>
2025-08-04 18:18:07 +0800

    Revert "Fix (#86)" (#87)
    This reverts commit 0fa0bc98049b3e11b016bbb962a43b30563d73a9.

--- src/Pass/Transform/DataFlow/Mem2Reg.cpp
+++ src/Pass/Transform/DataFlow/Mem2Reg.cpp
@@ -1,19 +1,10 @@
-#include <deque>
 #include <unordered_set>
 
 #include "Mir/Builder.h"
 #include "Pass/Analysis.h"
 #include "Pass/Transforms/DataFlow.h"
-#include "Pass/Util.h"
 
 using namespace Mir;
-
-namespace {
-std::string make_name(const std::string &prefix) {
-    static int id{0};
-    return prefix + std::to_string(++id);
-}
-}
 
 namespace Pass {
 void Mem2Reg::init_mem2reg() {
@@ -29,73 +20,54 @@ void Mem2Reg::init_mem2reg() {
                       user->to_string().c_str());
         }
         if (const auto load = std::dynamic_pointer_cast<Load>(inst); load && !load->get_block()->is_deleted()) {
-            use_instructions.insert(load);
+            use_instructions.emplace_back(load);
         }
         if (const auto store = std::dynamic_pointer_cast<Store>(inst); store && !store->get_block()->is_deleted()) {
-            def_instructions.insert(store);
-            def_blocks.insert(store->get_block());
+            def_instructions.emplace_back(store);
+            if (std::find(def_blocks.begin(), def_blocks.end(), store->get_block()) == def_blocks.end()) {
+                def_blocks.emplace_back(store->get_block());
+            }
         }
     }
-
-    // std::ostringstream oss;
-    // oss << "Def Insts:\n";
-    // for (const auto &inst: def_instructions) {
-    //     oss << "  " << inst->to_string() << "\n";
-    // }
-    // oss << "Def Blocks:\n";
-    // for (const auto &block: def_blocks) {
-    //     oss << "  " << block->get_name() << "\n";
-    // }
-    // oss << "Use Insts:\n";
-    // for (const auto &inst: use_instructions) {
-    //     oss << "  " << inst->to_string() << "\n";
-    // }
-    // log_debug("\n%s", oss.str().c_str());
 }
 
 void Mem2Reg::insert_phi() {
     std::unordered_set<std::shared_ptr<Block>> processed_blocks; // 已处理的基本块
-    std::deque worklist(def_blocks.begin(), def_blocks.end());
-
+    std::vector<std::shared_ptr<Block>> worklist = def_blocks;
     while (!worklist.empty()) {
         const auto x = worklist.front();
-        worklist.pop_front();
-
+        worklist.erase(worklist.begin());
         for (const auto &y: dom_info->graph(current_function).dominance_frontier.at(x)) {
-            if (processed_blocks.find(y) == processed_blocks.end()) {
-                processed_blocks.insert(y);
-                if (def_blocks.find(y) == def_blocks.end()) {
-                    worklist.push_back(y);
-                }
+            if (processed_blocks.find(y) != processed_blocks.end())
+                continue;
+            std::unordered_map<std::shared_ptr<Block>, std::shared_ptr<Value>> optional_map;
+            for (const auto &prev_block: cfg_info->graph(current_function).predecessors.at(y)) {
+                optional_map[prev_block] = nullptr;
+            }
+            const auto contain_type =
+                    std::static_pointer_cast<Type::Pointer>(current_alloc->get_type())->get_contain_type();
+            const auto phi = Phi::create(Builder::gen_variable_name(), contain_type, nullptr, optional_map);
+            phi->set_block(y, false);
+            y->get_instructions().insert(y->get_instructions().begin(), phi);
+            use_instructions.emplace_back(phi);
+            def_instructions.emplace_back(phi);
+            processed_blocks.insert(y);
+            if (std::find(def_blocks.begin(), def_blocks.end(), y) == def_blocks.end()) {
+                worklist.emplace_back(y);
             }
         }
-    }
-
-    for (const auto &b : processed_blocks) {
-        std::unordered_map<std::shared_ptr<Block>, std::shared_ptr<Value>> optional_map;
-        const auto contain_type =
-                std::static_pointer_cast<Type::Pointer>(current_alloc->get_type())->get_contain_type();
-
-        for (const auto &prev_block: cfg_info->graph(current_function).predecessors.at(b)) {
-            optional_map[prev_block] = std::make_shared<Value>(make_name("%phi_value_"), contain_type);
-        }
-
-        const auto phi = Phi::create(make_name("%phi_"), contain_type, nullptr, optional_map);
-        phi->set_block(b, false);
-        b->get_instructions().insert(b->get_instructions().begin(), phi);
-
-        use_instructions.insert(phi);
-        def_instructions.insert(phi);
     }
 }
 
 void Mem2Reg::rename_variables(const std::shared_ptr<Block> &block) {
     int stack_depth{0};
     const auto contain_type = std::static_pointer_cast<Type::Pointer>(current_alloc->get_type())->get_contain_type();
-
-    for (const auto &inst: block->get_instructions()) {
-        if (use_instructions.count(inst) && inst->get_op() == Operator::LOAD) {
-            const auto load = std::static_pointer_cast<Load>(inst);
+    for (auto it = block->get_instructions().begin(); it != block->get_instructions().end();) {
+        if (auto instruction = *it; instruction == current_alloc) {
+            it = block->get_instructions().erase(it);
+        } else if (const auto load = std::dynamic_pointer_cast<Load>(instruction);
+                   load &&
+                   std::find(use_instructions.begin(), use_instructions.end(), load) != use_instructions.end()) {
             std::shared_ptr<Value> new_value;
             if (!def_stack.empty()) {
                 new_value = def_stack.back();
@@ -107,39 +79,40 @@ void Mem2Reg::rename_variables(const std::shared_ptr<Block> &block) {
                 log_error("Unsupported type: %s", contain_type->to_string().c_str());
             }
             load->replace_by_new_value(new_value);
-        }
-        if (def_instructions.count(inst)) {
-            if (inst->get_op() == Operator::STORE) {
-                def_stack.push_back(inst->as<Store>()->get_value());
-            } else if (inst->get_op() == Operator::PHI) {
-                def_stack.push_back(inst->as<Phi>());
-            } else {
-                log_error("Invalid inst: %s", inst->to_string().c_str());
-            }
+            it = block->get_instructions().erase(it);
+        } else if (const auto store = std::dynamic_pointer_cast<Store>(instruction);
+                   store &&
+                   std::find(def_instructions.begin(), def_instructions.end(), store) != def_instructions.end()) {
+            const auto stored_value = store->get_value();
+            def_stack.emplace_back(stored_value);
+            store->clear_operands();
             ++stack_depth;
+            it = block->get_instructions().erase(it);
+        } else if (const auto phi = std::dynamic_pointer_cast<Phi>(instruction);
+                   phi && std::find(def_instructions.begin(), def_instructions.end(), phi) != def_instructions.end()) {
+            def_stack.emplace_back(phi);
+            ++stack_depth;
+            ++it;
+        } else {
+            ++it;
         }
     }
-
     // 第二遍遍历：更新后继块的Phi操作数
     for (const auto &succ_block: cfg_info->graph(current_function).successors.at(block)) {
-        for (const auto &inst: succ_block->get_instructions()) {
-            if (inst->get_op() != Operator::PHI)
-                break;
-
-            const auto phi = std::static_pointer_cast<Phi>(inst);
-            if (use_instructions.count(inst)) {
-                std::shared_ptr<Value> new_value;
-                if (!def_stack.empty()) {
-                    new_value = def_stack.back();
-                } else if (contain_type->is_int32()) {
-                    new_value = ConstInt::create(0);
-                } else if (contain_type->is_float()) {
-                    new_value = ConstFloat::create(0.0);
-                } else {
-                    log_error("Unsupported type: %s", contain_type->to_string().c_str());
-                }
-                phi->modify_operand(phi->get_optional_values().at(block), new_value);
+        const auto first_instruction = succ_block->get_instructions().front();
+        if (const auto phi = std::dynamic_pointer_cast<Phi>(first_instruction);
+            phi && std::find(use_instructions.begin(), use_instructions.end(), phi) != use_instructions.end()) {
+            std::shared_ptr<Value> new_value;
+            if (!def_stack.empty()) {
+                new_value = def_stack.back();
+            } else if (contain_type->is_int32()) {
+                new_value = ConstInt::create(0);
+            } else if (contain_type->is_float()) {
+                new_value = ConstFloat::create(0.0f);
+            } else {
+                log_error("Unsupported type: %s", contain_type->to_string().c_str());
             }
+            phi->set_optional_value(block, new_value);
         }
     }
     // 递归处理支配子树
@@ -161,7 +134,7 @@ void Mem2Reg::run_on_func(const std::shared_ptr<Function> &func) {
                 if (const auto contain_type =
                             std::static_pointer_cast<Type::Pointer>(alloc->get_type())->get_contain_type();
                     !contain_type->is_array()) {
-                    valid_allocs.push_back(alloc);
+                    valid_allocs.emplace_back(alloc);
                 }
             }
         }
@@ -170,30 +143,34 @@ void Mem2Reg::run_on_func(const std::shared_ptr<Function> &func) {
     for (const auto &alloc: valid_allocs) {
         current_alloc = alloc;
         current_function = func;
+        def_blocks.clear();
         init_mem2reg();
         insert_phi();
         rename_variables(current_function->get_blocks().front());
-        deleted_instructions.insert(alloc);
-        for (const auto &i : def_instructions) {
-            if (i->get_op() != Operator::PHI)
-                deleted_instructions.insert(i);
-        }
-        for (const auto &i : use_instructions) {
-            if (i->get_op() != Operator::PHI)
-                deleted_instructions.insert(i);
-        }
-        Utils::delete_instruction_set(Module::instance(), deleted_instructions);
-        deleted_instructions.clear();
     }
 }
 
 void Mem2Reg::transform(const std::shared_ptr<Module> module) {
-    set_analysis_result_dirty<ControlFlowGraph>(module);
     cfg_info = get_analysis_result<ControlFlowGraph>(module);
     dom_info = get_analysis_result<DominanceGraph>(module);
     for (const auto &func: *module) {
+        // 收集当前函数的所有Alloc指令
         run_on_func(func);
     }
+    current_alloc = nullptr;
+    current_function = nullptr;
+    cfg_info = nullptr;
+    dom_info = nullptr;
+    def_instructions.clear();
+    use_instructions.clear();
+    def_blocks.clear();
+    def_stack.clear();
+}
+
+void Mem2Reg::transform(const std::shared_ptr<Function> &func) {
+    cfg_info = get_analysis_result<ControlFlowGraph>(Module::instance());
+    dom_info = get_analysis_result<DominanceGraph>(Module::instance());
+    run_on_func(func);
     current_alloc = nullptr;
     current_function = nullptr;
     cfg_info = nullptr;
